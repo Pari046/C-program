2c3bfd74652b62455e9307598b0b891e2be89b2a
Parineeta <145686345+Pari046@users.noreply.github.com>
2023-12-03 10:51:56 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,11 @@
+/*addition of two numbers*/
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("sum of %d and %d is:%d",sum);
+    return 0;
+}
